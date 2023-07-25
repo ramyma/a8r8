b1665ca9f36a3082a820ef9b365afd016dc39b53
ramyma <ramy.mahmoudi@gmail.com>
2023-07-25 03:19:57 +0300

    Fix config

--- config/config.exs
+++ config/config.exs
@@ -61,7 +61,9 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 config :nx, default_backend: EXLA.Backend
-config :exla, :clients, cuda: [platform: :cuda, :host]
+config :exla, :clients,
+  cuda: [platform: :cuda],
+  host: [platform: :host]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
