0ed783dab21048943bdcf0319bd5c3cf3a49912b
ramyma <ramy.mahmoudi@gmail.com>
2023-09-19 03:16:52 +0300

    Change lora attention increment to 0.1

--- assets/js/src/components/Editor/CustomExtension.tsx
+++ assets/js/src/components/Editor/CustomExtension.tsx
@@ -99,7 +99,7 @@ export class CustomExtension extends NodeExtension<ExtrasOptions> {
             tr.setNodeAttribute(
               state.selection.from,
               "value",
-              formatToMaxDecimalPlaces(Math.min(3, parseFloat(value) + 0.01), 2)
+              formatToMaxDecimalPlaces(Math.min(3, parseFloat(value) + 0.1), 2)
             )
           );
         } else if (node?.type?.name === "attention") {
@@ -150,10 +150,7 @@ export class CustomExtension extends NodeExtension<ExtrasOptions> {
             tr.setNodeAttribute(
               state.selection.from,
               "value",
-              formatToMaxDecimalPlaces(
-                Math.max(-3, parseFloat(value) - 0.01),
-                2
-              )
+              formatToMaxDecimalPlaces(Math.max(-3, parseFloat(value) - 0.1), 2)
             )
             // .setSelection({
             //   $head: state.selection.$head,
