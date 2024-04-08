1d0b2f02d4bcc1c9e80f2cbd1b8e85aa98768e74
ramyma <ramy.mahmoudi@gmail.com>
2024-04-09 01:56:55 +0200

    Update one click installer batch file

--- one-click-installer-wsl.bat
+++ one-click-installer-wsl.bat
@@ -1,4 +1,4 @@
 wsl --install -d ubuntu --no-launch
 wsl -e bash -c "CLONE_REPO=true ./install.sh"
-cd .\a8r8\
-echo 'If instalaltion was successfull, run ".\run.bat" in the created "a8r8" folder to start the application'
+
+echo 'If instalaltion was successfull, run the "run.bat" file in the created "a8r8" folder to start the application'
