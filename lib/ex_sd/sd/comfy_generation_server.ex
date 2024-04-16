e19794faaaf58b06570bd8d11f668491e9ab9952
ramyma <ramy.mahmoudi@gmail.com>
2024-04-16 21:14:22 +0200

    Update comfy gen server init to fix crash

--- lib/ex_sd/sd/comfy_generation_server.ex
+++ lib/ex_sd/sd/comfy_generation_server.ex
@@ -14,7 +14,8 @@ defmodule ExSd.ComfyGenerationServer do
   def init(_args) do
     client_id = Ecto.UUID.generate()
 
-    %{backend: backend, is_connected: is_connected} = :sys.get_state(ExSd.SdSever, timeout: 5_000)
+    %{backend: backend, is_connected: is_connected} =
+      :sys.get_state(GenServer.whereis(ExSd.SdSever))
 
     if(backend == :comfy and is_connected,
       do: connect_to_websocket(client_id)
